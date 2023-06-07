055e45ca765b56c449f22d9d6af0c1f52e7741f2
PowerRiddler <karlo.baljak@gmail.com>
2023-06-07 19:01:32 +0200

    adding and removing from cart implemented

--- labos2/routes/cart.routes.js
+++ labos2/routes/cart.routes.js
@@ -18,22 +18,46 @@ function readCartData(pathToFile) {
         items.forEach((item) => {
             item['name'] = dataJson.categories.find(catId => catId.id == item.id.split('-')[0]).products.find(productId => productId.id == item.id).name;
         });
-        console.log("ITEMS: ",items);
+        /*console.log("ITEMS: ",items);*/
         return items;
     } catch (error) {
         throw new Error('Internal server error');
     }
 }
 
-
-
 /*GET cart main page*/
 router.get('/cart/getAll', (req, res) => {
     let shoppingCart = {
         name: "Checkout",
     };
-    var articlesAddedToCart = readCartData(path.join(__dirname, '../data/cartData.json'));
-    res.render('cart', {currentCategory: shoppingCart, cartItems: articlesAddedToCart });
-})
+    var productsAddedToCart = readCartData(path.join(__dirname, '../data/cartData.json'));
+    res.render('cart', {currentCategory: shoppingCart, cartItems: productsAddedToCart });
+});
+
+router.get('/cart/remove/:id(\\d+-\\d+)', (req, res) => {
+    let itemId = req.params.id;
+    let data = fs.readFileSync(path.join(__dirname, '../data/cartData.json'), 'utf8');
+    let cartDataJSON = JSON.parse(data);
+    const removeOne = cartDataJSON.products.find(item => item.id == itemId);
+    removeOne.quantity -=1;
+    fs.writeFile(path.join(__dirname, '../data/cartData.json'), JSON.stringify(cartDataJSON), 'utf8', err =>{
+        if(err) {res.status(500).send('Internal server error'); return;}
+        console.log("Removed 1 from item with ID:",itemId);
+        res.redirect('/cart/getAll');
+    });
+});
+
+router.get('/cart/add/:id(\\d+-\\d+)', (req, res) => {
+    let itemId = req.params.id;
+    let data = fs.readFileSync(path.join(__dirname, '../data/cartData.json'), 'utf8');
+    let cartDataJSON = JSON.parse(data);
+    const addOne = cartDataJSON.products.find(item => item.id == itemId);
+    addOne.quantity +=1;
+    fs.writeFile(path.join(__dirname, '../data/cartData.json'), JSON.stringify(cartDataJSON), 'utf8', err =>{
+        if(err) {res.status(500).send('Internal server error'); return;}
+        console.log("Added 1 to item with ID:",itemId);
+        res.redirect('/cart/getAll');
+    });
+});
 
 module.exports = router;
